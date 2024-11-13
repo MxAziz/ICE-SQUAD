0d06997ef6b8e07bf6a098dfcce92d5a4bf87230
MxAziz <hereabdulaziz@gmail.com>
2024-11-13 22:12:43 +0600

    display card

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { LiaFacebook } from "react-icons/lia";
 
-const Student = ({ img, title, description }) => {
+const Student = ({ img, title, description, fb }) => {
   return (
-    <div className="card card-compact bg-base-100 w-96 shadow-xl">
+    <div className="card card-compact bg-base-100 w-[370px] mx-auto shadow-xl">
       <figure>
-        <img className="h-[250px] w-11/12 mt-4 rounded-xl" src={img} />
+        <img className="h-[250px] w-5/6 mt-4 rounded-xl" src={img} />
       </figure>
       <div className="card-body text-center">
         <h2 className="text-xl font-bold text-center">{title}</h2>
@@ -14,9 +14,11 @@ const Student = ({ img, title, description }) => {
           District: {description}
         </p>
         <div className="card-actions justify-center">
-          <button className="btn btn-wide bg-[#2B3440] hover:bg-[#1a2534] text-white">
-            Facebook
-          </button>
+          <a href={fb}>
+            <button className="btn btn-wide bg-[#2B3440] hover:bg-[#1a2534] text-white">
+              Facebook
+            </button>
+          </a>
         </div>
       </div>
     </div>
--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -7,77 +7,44 @@ const Students = () => {
       <div className="">
         {/* banner */}
         <Banner></Banner>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
+        <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8 ">
           {/* student */}
-          <div className="card bg-base-100 lg:w-96 shadow-xl mx-auto">
-            <figure className=" p-6">
-              <img src="/mozahid.jpg" className="rounded-xl h-[200px]" />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Name: Mozahid </h2>
-              <h3>District: Dinajpur </h3>
-            </div>
-          </div>
-          <div className="card bg-base-100 lg:w-96 shadow-xl mx-auto">
-            <figure className=" p-6">
-              <img src="/mozahid.jpg" className="rounded-xl h-[200px]" />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Name: Mozahid </h2>
-              <h3>District: Dinajpur </h3>
-            </div>
-          </div>
-          <div className="card bg-base-100 w-96 shadow-xl">
-            <figure className=" p-6">
-              <img src="/mozahid.jpg" className="rounded-xl h-[200px]" />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Name: Mozahid </h2>
-              <h3>District: Dinajpur </h3>
-            </div>
-          </div>
-          <div className="card bg-base-100 w-96 shadow-xl mt-10">
-            <figure className=" p-6">
-              <img src="/mozahid.jpg" className="rounded-xl h-[200px]" />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Name: Mozahid </h2>
-              <h3>District: Dinajpur </h3>
-            </div>
-          </div>
-          <div className="card bg-base-100 w-96 shadow-xl mt-10">
-            <figure className=" p-6">
-              <img src="/mozahid.jpg" className="rounded-xl h-[200px]" />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Name: Mozahid </h2>
-              <h3>District: Dinajpur </h3>
-            </div>
-          </div>
-          <div className="card bg-base-100 w-96 shadow-xl mt-10">
-            <figure className=" p-6">
-              <img src="/mozahid.jpg" className="rounded-xl h-[200px]" />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Name: Mozahid </h2>
-              <h3>District: Dinajpur </h3>
-            </div>
-          </div>
-          {/* test */}
           <Student
             img={"/founder.jpg"}
             title="Muhammad Aziz"
             description="Chapai Nawabgonj."
+            fb="https://www.facebook.com/mxaziz10"
+          ></Student>
+          <Student
+            img="/nafi.jpg"
+            title="Abdullah Al Nafi"
+            description={"Bogura"}
+            fb="https://www.facebook.com/al.nafee.7"
           ></Student>
           <Student
             img={"/navin.jpg"}
             title="Navin Nawar"
-            description={"Shirajgonj"}
+            description={"Gazipur"}
+            fb="https://www.facebook.com/navin.nawar.79"
           ></Student>
           <Student
             img="/mozahid.jpg"
             title="Mozahidul Islam Mozahid"
             description={"Dinajpur"}
+            fb="https://www.facebook.com/mozahid.zahid.3975"
+          ></Student>
+          <Student
+            img="/leone.jpg"
+            title="Shahriar Mahmud Leone "
+            description={"khulna"}
+            fb="https://www.facebook.com/profile.php?id=61568144774135"
+          ></Student>
+
+          <Student
+            img="/evan.jpg"
+            title="Asibuzzaman Evan Sarker "
+            description={"Shirajgonj"}
+            fb="https://www.facebook.com/mdevan.sarker"
           ></Student>
 
           {/*  */}
