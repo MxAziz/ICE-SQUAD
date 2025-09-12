daef5ce240ca81b0aa06801e3fdcbe9644cea463
MxAziz <hereabdulaziz@gmail.com>
2025-09-12 22:43:07 +0600

    change my profile picture

--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -11,7 +11,7 @@ const Students = () => {
         {/*all student card container */}
         <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8 ">
           <Student
-            img={"/founder.jpg"}
+            img={"/aziz.png"}
             title="Muhammad Aziz"
             description="Chapai Nawabgonj."
             phone={8801518939563}
